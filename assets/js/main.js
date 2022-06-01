6094b789fe429bd89da6f26bcd13b078d99e8f1e
Sabrina <sabrinahmed47@gmail.com>
2022-06-01 18:05:01 +0200

    update html-css

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,13 @@
-console.log("Oui chef!")
+const express = require('express');
+const data = require('./data.json');
+const axios = require('axios');
+const cors = require('cors');
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("hello la team!").status(200);
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
