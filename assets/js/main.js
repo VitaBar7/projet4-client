77dce4767bc3ed3a927cb375f9592f6a02d94b67
Sabrina <sabrinahmed47@gmail.com>
2022-06-03 15:35:27 +0200

    final

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,105 @@
 const divResult = document.getElementById('results');
 
-axios.get("http://localhost:5050/api/planets")
-.then(response => response.data)
-.then(data => data.results.map(item => {
-
-    const card = document.createElement('div');
-    card.classList.add('card');
-   
-    const button = document.createElement('button');
-    button.classList.add('button');
-    button.textContent ="Plus d'infos..."
-    button.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif"
-    button.onclick = function(){
-        window.open("https://fr.wikipedia.org/wiki/Liste_des_astres_de_Star_Wars")
-    };
-       
-    const img = document.createElement('img');
-    img.src = "assets/images/mort.png"
+const getPlanets = () => {
     
-    const title = document.createElement('h2');
-    title.innerHTML = item.name;
+    divResult.innerHTML = '';
+    axios.get("http://localhost:5050/api/planets")
+    .then(response => response.data)
+    .then(data => data.results.map(item => {
+    
+        const card = document.createElement('div');
+        card.classList.add('card');
+       
+        const button = document.createElement('button');
+        button.classList.add('button');
+        button.textContent ="Plus d'infos..."
+        button.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif"
+        button.onclick = function(){
+            window.open("https://fr.wikipedia.org/wiki/Liste_des_astres_de_Star_Wars")
+        };
+                   
+        const title = document.createElement('h2');
+        const blabla = document.createElement('p');
+        const climate = document.createElement('p');
+        const terrain = document.createElement('p');
 
-    card.appendChild(img);
-    card.appendChild(button);
-    card.appendChild(title);
-    divResult.appendChild(card);
-}))
+
+        // document.querySelector('#results').innerHTML +=  '<h3>climate: ' + data.results[4] + '</h3';
+
+        title.innerHTML = item.name;
+        blabla.innerHTML = 'Rotation period: ';
+        blabla.innerHTML += item.rotation_period;
+
+        terrain.innerHTML = 'Terrain: ';
+        terrain.innerHTML += item.terrain;
+
+        climate.innerHTML = 'Climate: ';
+        climate.innerHTML += item.climate;
+
+        card.appendChild(title);
+        card.appendChild(blabla);
+        card.appendChild(climate);
+        card.appendChild(terrain);
+        card.appendChild(button);
+
+        divResult.appendChild(card);
+    }));
+};
+
+getPlanets();
+
+ 
+document.getElementById('get-planets').addEventListener('click', e => {
+    
+    getPlanets();
+});
+
+
+
+
+////////////////////////// WEB SCRAPING ///////////////////////////
+
+// axios.get("http://localhost:5050/api/characters")
+//     .then(response => response.data)
+//     .then(data => data.results.map(item => {
+
+//         const personnages = data.name
+
+//         document.querySelector('#results').innerHTML += personnages
+
+//     }));
+
+const getCharacters = () => {
+    
+    divResult.innerHTML = '';
+    axios.get("http://localhost:5050/api/characters")
+    .then(response => response.data)
+    .then(data => data.results.map(item => {
+    
+        const card = document.createElement('div');
+        card.classList.add('card');
+       
+        const button = document.createElement('button');
+        button.classList.add('button');
+        button.textContent ="Plus d'infos..."
+        button.style.fontFamily = "Verdana, Geneva, Tahoma, sans-serif"
+        button.onclick = function(){
+            window.open("https://fr.wikipedia.org/wiki/Liste_des_astres_de_Star_Wars")
+        };
+                   
+        const title = document.createElement('h2');
+
+
+
+        // document.querySelector('#results').innerHTML +=  '<h3>climate: ' + data.results[4] + '</h3';
+
+        title.innerHTML = item.name;
+
+
+        card.appendChild(title);
+       
+        card.appendChild(button);
+
+        divResult.appendChild(card);
+    }));
+};
